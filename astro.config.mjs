3187f6d9bd21d4ec501c5c246adbe62aa6ca03f7
sylenthmen <manuelene26@gmail.com>
2024-03-14 21:55:10 -0600

    s

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true}
+    webAnalytics: { enabled: true }
   }),
   integrations: [tailwind()]
 });
